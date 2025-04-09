ea6303aa7483cee5d2199f56e48374e9d9247305
sivarubini5 <163725752+sivarubini5@users.noreply.github.com>
2025-04-09 21:55:03 +0530

    Create Task.java

---
+++ model/Task.java
@@ -0,0 +1,17 @@
+package com.example.todo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed;
+}
